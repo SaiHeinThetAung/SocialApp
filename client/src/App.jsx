a1a07d783a0d58a378fdc57bbe1a9a587986951f
saihein <saiheinthetaung669@gmail.com>
2024-04-18 13:03:55 +0630

    fourth cm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,26 @@ import Home from './Pages/Home'
 import {ToastContainer} from 'react-toastify'
 import Login from './Pages/Auth/Login'
 import Register from './Pages/Auth/Register'
+import { AuthContextProvider } from './Context/AuthContext'
+import { Profile } from './Pages/Profile'
+import RedirectAuth from './Pages/RedirectNoAuth/RedirectAuth'
 const App = () => {
   return (
    <>
-       <Routes>
+       <AuthContextProvider>
+          <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/register' element={<Register/>}/>
           <Route path='/login' element={<Login/>}/>
+          <Route path='/profile' element={
+          <RedirectAuth>
+              <Profile/>
+          </RedirectAuth>
+          }/>
+
        </Routes>
-       <ToastContainer
-          autoClose={1000}
-        />
+       <ToastContainer autoClose={1000} />
+       </AuthContextProvider>
    </>
   )
 }
