93ca660642293aaf111820070955f179a2204edd
saihein <saiheinthetaung669@gmail.com>
2024-05-17 20:16:11 +0630

    17/5

---
+++ client/src/Components/PageLoader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const PageLoader = () => {
+  return (
+    <div style={{marginLeft:'400px',marginTop:'200px'}}>
+            <div class="spinner-grow text-white" role="status">
+            <span class="sr-only">Loading...</span>
+            </div>
+            <div class="spinner-grow text-white" role="status">
+            <span class="sr-only">Loading...</span>
+            </div>
+            <div class="spinner-grow text-success" role="status">
+            <span class="sr-only">Loading...</span>
+            </div>
+    </div>
+  )
+}
